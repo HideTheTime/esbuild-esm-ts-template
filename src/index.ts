6d5f5e2eeac9555357f3d5d8351718c8874f829a
_____W_somnium____ <56722571+HideTheTime@users.noreply.github.com>
2023-03-13 19:15:33 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ import fastify, {
   FastifyInstance,
 } from "fastify";
 
-const startServer = async (): Promise<FastifyInstance> => {
+const startServer = async (): Promise<FastifyInstance> =>
+{
   const app = fastify();
 
-  app.get("/", async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> => {
-    return reply.send({ hello: "wor22ld22222" });
+  app.get("/", async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> =>
+  {
+    return reply.send({ hello: "wor2221l2d22223221" });
   });
 
   return app;
@@ -18,3 +20,5 @@ const startServer = async (): Promise<FastifyInstance> => {
 startServer()
   .then((app) => app.listen(3333))
   .catch(console.error);
+
+export * from './test'
